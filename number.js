97626486aa4169bce45f33def2fe4a1a996f6616
Jignesh <jigsvadiyatar6557@gmail.com>
2022-11-30 14:55:09 +0530

    change

---
+++ number.js
@@ -0,0 +1,12 @@
+let num =15; 
+    count = 0;
+    for(i=0; i<=num ; i++){
+        if(num%i==1){
+            count++;
+        }    
+       } if(count==2){
+        console.log("Yes")
+        
+        }else{
+         console.log("No")
+        }
